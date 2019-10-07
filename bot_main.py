623d141825002b6c101a2888d55cb0f14f48b59c
OXFORDX <42910683+OXFORDX@users.noreply.github.com>
2019-10-08 02:31:29 +0300

    Hell of Classes

--- bot_main.py
+++ bot_main.py
@@ -2,39 +2,49 @@ import json
 import os
 
 
+class Checker:
+    def __init__(self, std):
+        self.std = std
+
+    def __new__(cls, std):
+        def __check():
+            self = std
+            print(self.data)
+            try:
+                if self.data['Login'] == self.logpass[0]:
+                    if self.data['Passwd'] == self.logpass[1]:
+                        print('Access granted')
+                        return True
+                    else:
+                        print('Invalid passwd')
+                        return False
+                else:
+                    print('Invalid login')
+                    return False
+            except TypeError:
+                print('Користувача не знайдено.')
+
+        if __check():
+            return std
+        else:
+            return None
+
+
 class Archive:
-    def __init__(self, fname, lname, logpass):
+    def __init__(self, fname, lname, clsname):
         self.fname = fname
         self.lname = lname
-        self.logpass = logpass
+        self.clsname = clsname
+
+        self.isArchive = True
         if 'data.json' in os.listdir():
             self.data = self.__readjson()
 
         else:
             self.__createjson()
-        if not self.__checker():
-            self.__del__()
 
     def getData(self):
-        return self.data[self.logpass[2]][f'{self.fname} {self.lname}']
-
-    def __checker(self):
-        try:
-            if self.data[self.logpass[2]][f'{self.fname} {self.lname}'] \
-                    and self.data[self.logpass[2]][f'{self.fname} {self.lname}']['Login'] == self.logpass[0]:
-                if self.data[self.logpass[2]][f'{self.fname} {self.lname}']['Passwd'] == self.logpass[1]:
-                    print('Access granted')
-                    return True
-                else:
-                    print('Invalid passwd')
-                    self.__del__()
-                    return
-            else:
-                print('Invalid login')
-                self.__del__()
-                return
-        except TypeError:
-            print('Користувача не знайдено.')
+        return self.data[self.clsname][f'{self.fname} {self.lname}']
 
     def writejson(self, data):
         with open('data.json', 'w') as file:
@@ -48,7 +58,7 @@ class Archive:
 
             json_r = json.load(file)
             if not json_r:
-                self.__del__()
+                self.isArchive = False
                 print('Json is empty')
                 return
             else:
@@ -56,34 +66,36 @@ class Archive:
                 print(data)
         return data
 
-    def __del__(self):
-        print('Destructor')
-
 
 class Person:
-    def __init__(self, fname, lname, logpass):
+    def __init__(self, fname, lname, ):
         self.fname = fname
         self.lname = lname
-        self.archive = Archive(fname, lname, logpass)
+        self.logpass = self.__login()
+        self.archive = Archive(fname, lname, self.__class__.__name__)
         self.data = self.archive.getData()
 
-    def saveChanges(self):
-        self.archive.writejson()
-
-
-class Students(Person):
-    def __init__(self, firstname, lastname):
-        Person.__init__(self, firstname, lastname, self.__login())
+    # def saveChanges(self):
+    #     self.archive.writejson()
 
     def __login(self):
         log = input('Введіть логін: ')
         passwd = input('Введіть пароль: ')
-        return log, passwd, self.__class__.__name__
+        return log, passwd
+
+
+class Students(Person):
+    def __init__(self, firstname, lastname):
+        Person.__init__(self, firstname, lastname)
 
     def WatchMarks(self):
         for k, v in self.data['Marks'].items():
             print(f'{k}: {v}')
 
 
-std = Students('Orlov', 'Volodya')
+std = Checker(Students('Orlov', 'Volodya'))
 std.WatchMarks()
+
+"""
+std = Checker(Students('Orlov', 'Volodya')) //None or Student
+"""
